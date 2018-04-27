1afe1cad4ef2c41b15c1050800cb5a0af8cff9d1
kamalasaurus <kamalasaurus@gmail.com>
2018-04-27 01:11:39 -0400

    some interface and world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+fn main(source: &str) -> String {
+  println!("Hello, world!");
 }
+
+enum Token {
+  OR,
+  AND,
+  NEAR,
+  NOTNEAR,
+  WITH,
+  NOTWITH,
+  NOT,
+  EXCLUDE,
+  LEFTPAREN,
+  RIGHTPAREN,
+  TERM,
+  WILDCARD,
+  METADATA
+}
+
+fn parse(source: &str) -> Vec<Token> {
+
+}
+
